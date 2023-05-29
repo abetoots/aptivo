12a66254c2c96921a67af6461892ab401a7def13
Abe Caymo <caymo.abesuni@gmail.com>
2023-05-29 12:16:52 +0800

    add more services, update typography on large screens
    - add contact us cards

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -1,15 +1,22 @@
+//Components
 import Head from "next/head";
 import Link from "next/link";
-import { buttonVariants } from "~/components/Button";
 import Header from "~/components/Header";
-import { cn } from "~/lib/utils";
 import HandDrawMailSvg from "~/assets/handdrawn-mail.svg";
 import PaydaySvg from "~/assets/payday.svg";
 import HandDrawnAccountingSvg from "~/assets/handdrawn-accounting.svg";
 import HandDrawnAssistantSvg from "~/assets/handdrawn-assistant.svg";
 import HandDrawnGraphicDesignSvg from "~/assets/handdrawn-graphic-design.svg";
 import HandDrawnWebDesignSvg from "~/assets/handdrawn-web-design.svg";
+import HandDrawnColdCalllingSvg from "~/assets/handdrawn-cold-calling.svg";
+import HandDrawnCustomerSupportSvg from "~/assets/handdrawn-customer-support.svg";
+import HandDrawnLeadGenerationSvg from "~/assets/handdrawn-lead-generation.svg";
+import HandDrawnSocialMediaMarketingSvg from "~/assets/handdrawn-social-media-marketing.svg";
+
+//Misc
 import React from "react";
+import { cn } from "~/lib/utils";
+import { buttonVariants } from "~/components/Button";
 
 const Learn = () => {
   return (
@@ -24,7 +31,7 @@ const Learn = () => {
       <Header className="z-10">
         <div className="ml-auto ">
           <nav className="flex items-center gap-6">
-            <Link href="/features">Features</Link>
+            <Link href="/contact">Contact Us</Link>
             <Link
               href="/login"
               className={cn(buttonVariants({ variant: "accent" }))}
@@ -34,12 +41,12 @@ const Learn = () => {
           </nav>
         </div>
       </Header>
-      <main className="mt-24 px-6">
+      <main className="mt-24 p-6">
         <section className="mb-24 text-center">
-          <h1 className="mb-6 text-3xl font-extrabold">
+          <h1 className="mb-6 text-3xl font-extrabold md:text-6xl">
             Building wealth for business owners
           </h1>
-          <p className="text-sm leading-loose">
+          <p className="text-sm leading-loose md:text-xl">
             Aptivo is everything you need to build wealth as an entrepreneur,
             freelancer, consultant, or independent business owner. We serve
             entrepreneurs with the tools, services, and network to transform
@@ -47,7 +54,9 @@ const Learn = () => {
           </p>
         </section>
         <section className="mb-24">
-          <h2 className="mb-3 text-xl font-bold">Explore Our Products</h2>
+          <h2 className="mb-3 text-xl font-bold md:text-3xl">
+            Explore Our Products
+          </h2>
           <h3 className="mb-6 text-lg font-semibold">
             Use our products and tools to accelerate your business.
           </h3>
@@ -83,14 +92,28 @@ const Learn = () => {
                 </div>
               )}
             />
+            <Card
+              title="Have a specific tool in mind?"
+              description="Let us know what you need."
+              renderAfter={() => (
+                <Link
+                  href="/contact"
+                  className={cn(buttonVariants({ variant: "default" }))}
+                >
+                  Contact Us
+                </Link>
+              )}
+            />
           </div>
         </section>
-        <section>
-          <h2 className="mb-3 text-xl font-bold">Explore Our Services</h2>
+        <section className="mb-24">
+          <h2 className="mb-3 text-xl font-bold md:text-3xl">
+            Explore Our Services
+          </h2>
           <h3 className="mb-6 text-lg font-semibold">
             Work with our team to handle your business needs.
           </h3>
-          <div className="flexrow flex grid-cols-2 flex-col items-center justify-center gap-10 md:grid lg:grid-cols-4">
+          <div className="flexrow flex grid-cols-2 flex-col justify-center gap-10 md:grid lg:grid-cols-4">
             <Card
               renderImg={() => (
                 <div className="mx-auto mb-6 w-1/2">
@@ -110,6 +133,22 @@ const Learn = () => {
             <Card
               renderImg={() => (
                 <div className="mx-auto mb-6 w-1/2">
+                  <HandDrawnColdCalllingSvg />
+                </div>
+              )}
+              title="Cold Calling"
+            />
+            <Card
+              renderImg={() => (
+                <div className="mx-auto mb-6 w-1/2">
+                  <HandDrawnCustomerSupportSvg />
+                </div>
+              )}
+              title="Customer Support"
+            />
+            <Card
+              renderImg={() => (
+                <div className="mx-auto mb-6 w-1/2">
                   <HandDrawnGraphicDesignSvg />
                 </div>
               )}
@@ -118,10 +157,38 @@ const Learn = () => {
             <Card
               renderImg={() => (
                 <div className="mx-auto mb-6 w-1/2">
+                  <HandDrawnLeadGenerationSvg />
+                </div>
+              )}
+              title="Lead Generation"
+            />
+            <Card
+              renderImg={() => (
+                <div className="mx-auto mb-6 w-1/2">
+                  <HandDrawnSocialMediaMarketingSvg />
+                </div>
+              )}
+              title="Social Media Marketing"
+            />
+            <Card
+              renderImg={() => (
+                <div className="mx-auto mb-6 w-1/2">
                   <HandDrawnWebDesignSvg />
                 </div>
               )}
               title="Web Design"
+            />
+            <Card
+              title="Need a specific service?"
+              description="Let us know what you need."
+              renderAfter={() => (
+                <Link
+                  href="/contact"
+                  className={cn(buttonVariants({ variant: "default" }))}
+                >
+                  Contact Us
+                </Link>
+              )}
             />
           </div>
         </section>
